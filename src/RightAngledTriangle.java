b0d75ba99d88cbb43ea6e3573e991483f64de1f2
Ilia <ilia.klodin@student.griffith.ie>
2025-03-18 22:23:41 +0000

    Added the RightAngledTriangle child class with method stubs

---
+++ src/RightAngledTriangle.java
@@ -0,0 +1,45 @@
+public class RightAngledTriangle extends Shape {
+
+    private double leg1; // attribute for the one of the non-hypotenuse sides of the right-angled triangle
+    private double leg2; // attribute for the other non-hypotenuse side of the right-angled triangle
+
+    public RightAngledTriangle(String name, double base, double height) { // constructor where we can set the legs of the right triangle
+        super(name);
+        this.leg1 = base;
+        this.leg2 = height;
+    }
+
+    /*
+    getters and setters for the base and height attributes
+     */
+    public double getLeg1() {
+        return leg1;
+    }
+
+    public void setLeg1(double leg1) {
+        this.leg1 = leg1;
+    }
+
+    public double getLeg2() {
+        return leg2;
+    }
+
+    public void setLeg2(double leg2) {
+        this.leg2 = leg2;
+    }
+
+    @Override
+    public double area() { // implementation of the abstract method area from the Shape class
+        return 0.0;
+    }
+
+    @Override
+    public double perimeter() { // implementation of the abstract method perimeter from the Shape class
+        return 0.0;
+    }
+
+    @Override
+    public String toString() { // updated toString method to include the area and perimeter of the right-angled triangle
+        return super.toString() + "\nArea: " + area() + "\nPerimeter: " + perimeter();
+    }
+}
